f6d328b93108a683638f02dc34e12d9143d86bee
Rickard Englund <rickard.englund@gmail.com>
2019-11-01 18:51:28 +0100

    Implement ordinary least square

--- src/main.js
+++ src/main.js
@@ -1,8 +1,72 @@
-import Vue from 'vue'
-import App from './App.vue'
+import P5 from 'p5'
 
-Vue.config.productionTip = false
+const script = function (p5) {
+    let coords = [];
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    // NOTE: Set up is here
+    p5.setup = () => {
+        p5.createCanvas(500, 500);
+        // for (let i = 20; i < p5.width; i += 20) {
+        //     coords.push({
+        //         x: i,
+        //         y: p5.height - 30 + i * (-0.5)
+        //     })
+        // }
+    };
+
+    // NOTE: Draw is here
+    p5.draw = () => {
+        p5.background(250);
+        for (let i = 0; i < coords.length; i++) {
+          p5.stroke(0, 0, 0, 150);
+          p5.strokeWeight(3);
+          p5.circle(coords[i].x, coords[i].y, 10)
+        }
+
+        if (coords.length > 1) {
+            let equation = ordinaryLeastSquare(coords);
+            p5.stroke(200, 0, 0, 200);
+            p5.line(0, straightLine(equation, 0),
+                p5.width, straightLine(equation, p5.width));
+
+            p5.textSize(32);
+            p5.stroke(0);
+          p5.strokeWeight(1);
+            p5.text(toText(equation), 15, 30)
+        }
+    };
+
+    p5.mouseClicked = () => {
+        coords.push({
+            x: p5.mouseX,
+            y: p5.mouseY
+        });
+    }
+};
+new P5(script);
+
+function straightLine(equation, x) {
+    return equation.k * x + equation.m
+}
+
+function toText(equation) {
+    const precision = 2
+    return `y = ${equation.k.toFixed(precision)}x + ${equation.m.toFixed(precision)}`
+}
+
+function ordinaryLeastSquare(coords) {
+    let xMean = coords.map(c => c.x).reduce((total, e) => total + e) / coords.length;
+    let yMean = coords.map(c => c.y).reduce((total, e) => total + e) / coords.length;
+    let nominator = 0;
+    let denominator = 0;
+    for (let i = 0; i < coords.length; i++) {
+        nominator += (coords[i].x - xMean) * (coords[i].y - yMean);
+        denominator += Math.pow(coords[i].x - xMean, 2)
+    }
+    let slope = nominator / denominator;
+    return {
+        k: slope,
+        m: yMean - slope * xMean
+    }
+}
+
